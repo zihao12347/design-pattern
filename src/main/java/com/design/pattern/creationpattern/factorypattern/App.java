1864fe0284ef4c2b2287033cdd9ef433964fcd29
yanghao <3063972920@qq.com>
2022-04-01 09:33:00 +0800

    -yh 创建者模式-工厂模式

---
+++ src/main/java/com/design/pattern/creationpattern/factorypattern/App.java
@@ -0,0 +1,9 @@
+package com.design.pattern.creationpattern.factorypattern;
+
+/**
+ * @Author:aaa
+ * @Date:18:21
+ * @Version 1.0
+ */
+public class App {
+}
---
+++ src/main/java/com/design/pattern/creationpattern/factorypattern/simplefactory/APP.java
@@ -0,0 +1,9 @@
+package com.design.pattern.creationpattern.factorypattern.simplefactory;
+
+/**
+ * @Author:aaa
+ * @Date:18:42
+ * @Version 1.0
+ */
+public class APP {
+}
